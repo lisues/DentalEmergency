d6ffd0e2010363813d2e1125b8de66944dcba204
lisues <lisuejocelynshe@yahoo.com>
2017-09-05 10:51:03 -0700

    Clean up debugging statements

--- DentalEmergency/DirectionsViewController.swift
+++ DentalEmergency/DirectionsViewController.swift
@@ -98,18 +98,12 @@ class DirectionsViewController: UIViewController, MKMapViewDelegate {
 
                 DispatchQueue.main.async() {
                     if let  unwrappedResponse = response {
-                      //  self.miles.backgroundColor = UIColor.blue
-                        //UIColor(red: 0, green: 0.6039, blue: 0.9294, alpha: 1.0)
-                        self.miles.backgroundColor = UIColor(red: 0, green: 0.851, blue: 0.9294, alpha: 1.0)
-        self.showRoute( unwrappedResponse )
+                    self.miles.backgroundColor = UIColor(red: 0, green: 0.851, blue: 0.9294, alpha: 1.0)
+                        self.showRoute( unwrappedResponse )
                         self.setMapRegion( sourceLocation: self.myLocation!, destinationLocation: officeLocation! )
                         let distance = unwrappedResponse.routes[0].distance * 0.000621371
                         self.miles.text = "\(Double(round(100*distance)/100)) miles"
                         self.mapView.add(unwrappedResponse.routes[0].polyline)
-                      //  var boundingRegion = unwrappedResponse.routes[0].polyline.boundingMapRect
-                      //  boundingRegion.size.height = boundingRegion.size.height*1.2
-                      //  boundingRegion.size.width = boundingRegion.size.width*1.2
-                       // self.mapView.setVisibleMapRect(boundingRegion, animated: true)
                     }
                 }
             }
@@ -189,9 +183,6 @@ class DirectionsViewController: UIViewController, MKMapViewDelegate {
 
     func showRoute(_ response: MKDirectionsResponse) {
         for route in response.routes {
-  print("-------------------------")
-  print("route: \(route)")
-  print("try to create a new branch ")
             mapView.add(route.polyline,
                          level: MKOverlayLevel.aboveRoads)
             for step in route.steps {
